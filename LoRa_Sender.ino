8402146656fd9451768198509b6ed0b18b51f5d9
Hank Dikeman <48107082+hankdikeman@users.noreply.github.com>
2020-11-25 15:39:15 -0600

    added sensor input and send loop

--- LoRa_Sender.ino
+++ LoRa_Sender.ino
@@ -1,11 +1,31 @@
 #include <LoRa.h>
+#define inputpin_1 A0
+int sensorvalue = -10;
 
 void setup() {
-  // put your setup code here, to run once:
+  // start serial and set Baud rate to 9600 bps
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("LoRa Sender Node Test");
 
+  // initialize the LoRa sender protocol, kill program if not
+  if(!LoRa.begin(915E6)){
+    Serial.println("LoRa Shield Connection Failed");
+    while(1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read in analog signal from sensor
+  sensorvalue = analogRead(inputpin_1);
+  Serial.print("Reading: ");
+  Serial.println(sensorvalue);
   
+  // initialize packet and send it
+  LoRa.beginPacket();
+  LoRa.print(sensorvalue);
+  LoRa.endPacket();
+  
+  // delay between packet sends
+  delay(5000);
 }
